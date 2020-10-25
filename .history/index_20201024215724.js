004c427170854e03c08c715058fe7634250971a2
Phil Odinga <phil.odinga@astechcongo.com>
2020-10-25 00:46:23 -0500

    deployment comit

--- .history/index_20201024215724.js
+++ .history/index_20201024215724.js
@@ -5,7 +5,7 @@ const app = Restify.createServer({
     name: 'philia recipe engine'
 })
 
-const token = '2w8BANf1PMdNdruoYmHScelZCDkpDCFMoZAdcvgaa7'
+const token = 'abc12345'
 const bot = new methods('EAANz0eBh2w8BANf1PMdNdruoYmHScelZCDkpDCFMoZAdcvgaa7pHeOKsxLDEIN9vDaDoEZAHfK9GcsQdjFe2GTG32z1oCw7FxWZA9AFW34H2LQp1Opx9b7fQZBC3cyntPfriqDUmEYBm1VG2i60tZBHtBZABN6Wl5RTuw7TGlm7p33Gv20W5zCKwLsBYW9ljysZD')
 
 app.use(Restify.plugins.jsonp())
